a3d90388ddc36f0797e33bdc89343af018855a26
ToolsAO <150697798+ToolsAO@users.noreply.github.com>
2023-11-16 12:42:20 +0000

    test

--- src/routes/gearBuilder/+page.server.js
+++ src/routes/gearBuilder/+page.server.js
@@ -1,5 +1,12 @@
+async function fetchText(url) {
+    let response = await fetch(url);
+    let responseText = await response.text();
+
+    return responseText;
+}
+
 export async function load() {
 	return {
-		number: await fetch("https://toolsao.vercel.app/api/test").text()
+		number: await fetchText("https://toolsao.vercel.app/api/test")
 	};
 }
