dd0168e688ea78cd9d294ffba0578e08030156dc
ToolsAO <150697798+ToolsAO@users.noreply.github.com>
2023-12-15 02:02:04 +0000

    test

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -57,7 +57,7 @@
 	<div class="grid gap-6 mb-6 grid-cols-1">
 		<div>
 			<label for="pass" class="block mb-2 text-sm font-medium text-white dark:text-gray-900">Password</label>
-			<input type="text" id="pass" name="pass" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" bind:value={pass}>
+			<input type="password" id="pass" name="pass" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" bind:value={pass}>
 		</div>
 		<div>
 			{#each data.items as item}
